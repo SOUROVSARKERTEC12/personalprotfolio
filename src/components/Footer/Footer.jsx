c3807be15338fb6ab9b45b911c2265f879c83718
SOUROV <sourovsarker005@gmail.com>
2023-02-13 13:47:02 +0600

    Readme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>devilsourov@gmail.com</span>
+        <span>saurabhsarker008@gmail.com</span>
         <div className="f-icons">
             <a href="https://www.instagram.com/sourovsaurabh/">
                 <Insta color="white" size={"3rem"} />
